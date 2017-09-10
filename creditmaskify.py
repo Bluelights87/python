fd08c0e12ac9b84027a38776eeeda3556dc08ecb
Josh Caldwell <Josh@Joshs-MacBook-Pro.local>
2017-09-09 23:00:48 -0500

    Updated to remove debugging

--- creditmaskify.py
+++ creditmaskify.py
@@ -3,7 +3,7 @@ def maskify(cc):
   numChar = len(str(cc))
   maskVal = numChar - 4
   creditVal = [int(d) for d in str(cc)]
-  print(creditVal)
+##  print(creditVal)
   maskedCard = '#' * maskVal + str(creditVal[numChar - 4]) + str(creditVal[numChar - 3]) + str(creditVal[numChar - 2]) + str(creditVal[numChar - 1])
   print(maskedCard)
 ##  print('#' * maskVal + str(creditVal[numChar - 4]) + str(creditVal[numChar - 3]) + str(creditVal[numChar - 2]) + str(creditVal[numChar]))
